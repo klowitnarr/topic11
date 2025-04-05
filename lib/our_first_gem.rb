6eb120da07eb64bdeb3e47c4cf7480ced3c45b69
nickolaykrasilov <krasilov.kolya@yandex.ru>
2025-04-05 13:32:31 +0300

    Fixed bugs with functions

--- lib/our_first_gem.rb
+++ lib/our_first_gem.rb
@@ -3,56 +3,52 @@
 require_relative "our_first_gem/version"
 
 module OurFirstGem
-  # extend self
   class Error < StandardError; end
-  # Your code goes here...
 
-  # круг, прямоугольник, треугольник
-
-  # круг
-  def  circle(radius)
+  # Круг
+  def self.circle(radius)
     if radius.positive?
       s = Math::PI * radius**2
       puts "Площадь круга с радиусом #{radius} равна #{s.round(2)}"
+      return s.round(2)  
     else
       puts "Ошибка: радиус должен быть положительным числом"
+      return nil
     end
   rescue ArgumentError, TypeError
     puts "Ошибка: неверный тип аргументов"
+    return nil
   end
 
-  # прямоугольник
-  def (a, b = nil)
-    # Если передан один аргумент (квадрат)
+  # Прямоугольник (или квадрат, если указана одна сторона)
+  def self.rectangle(a, b = nil)
     b ||= a
     if a.to_f <= 0 || b.to_f <= 0
-      return puts "Ошибка: стороны должны быть положительными числами"
+      puts "Ошибка: стороны должны быть положительными числами"
+      return
     end
     area = a.to_f * b.to_f
-    # ?
     formatted_area = area == area.to_i ? area.to_i : area.round(2)
-
     puts "Площадь прямоугольника #{a} × #{b} = #{formatted_area}"
-  # ?
   rescue ArgumentError, TypeError
     puts "Ошибка: неверный тип аргументов"
   end
 
-  # треугольник
-  def triangle(a, b, c = nil, angle = nil)
+  # Треугольник (по трём сторонам или двум сторонам и углу)
+  def self.triangle(a, b, c = nil, angle = nil)
     if !c.nil? && angle.nil?
-      # Расчет по трем сторонам (формула Герона)
+      # Формула Герона
       if valid_triangle?(a, b, c)
         p = (a + b + c) / 2.0
         area = Math.sqrt(p * (p - a) * (p - b) * (p - c))
-        puts "Площадь треугольника по трем сторонам равна #{area.round(2)}"
+        puts "Площадь треугольника по трём сторонам равна #{area.round(2)}"
       else
         puts "Ошибка: такого треугольника не существует"
       end
     elsif c.nil? && angle.nil?
       puts "Ошибка: для расчета по двум сторонам и углу укажите угол"
     elsif c.nil? && !angle.nil?
-      # Расчет по двум сторонам и углу между ними
+      # По двум сторонам и углу между ними
       if valid_sides_and_angle?(a, b, angle)
         angle_radians = angle * Math::PI / 180.0
         area = 0.5 * a * b * Math.sin(angle_radians)
@@ -67,22 +63,19 @@ module OurFirstGem
     puts "Ошибка: неверный тип аргументов"
   end
 
-  # --- вспопомогательные (для треугольника) ---
-  def valid_triangle?(a, b, c)
+  # --- Вспомогательные методы для треугольника ---
+  def self.valid_triangle?(a, b, c)
     a > 0 && b > 0 && c > 0 &&
       a + b > c &&
       a + c > b &&
       b + c > a
   end
 
-  def valid_sides_and_angle?(a, b, angle)
+  def self.valid_sides_and_angle?(a, b, angle)
     a > 0 && b > 0 && angle > 0 && angle < 180
   end
 
-  # --------------------------------------------------
-
-  # написали на паре код для примера
-  # привет на разных языках
+  # --- Тестовые методы с пары
   def self.eng
     "Hello"
   end
@@ -99,18 +92,3 @@ module OurFirstGem
     "Ciao"
   end
 end
-
-# после того как все спеки пропишем
-# rake build
-# rake install
-
-# может ругаться на рубокоп
-# тогда надо его отдельно установить
-
-# ! ! !
-# хотим запустить:
-# rake install
-# rake build
-# в irb :
-# require 'our_first_gem'
-# OurFirstGem.<method>
